4fed92d720714b0543d49313a399abc44a6d1f22
thomas1399 <gurtlerthomas99@gmail.com>
2020-04-21 16:54:52 +0200

    Finished editing functionality

--- Assignment 4/assignment4/src/components/CityComponent.js
+++ Assignment 4/assignment4/src/components/CityComponent.js
@@ -1,14 +1,102 @@
 import React from "react"
 
-function CityComponent(props){
-    return(
-        <div>
-            <h3>{props.item.country + "     " + props.item.city}</h3>
-            <img src={props.item.image} alt="something"></img>
-            <p>{props.item.description}</p>
-            <p>Rating: {props.item.rating}</p>
-        </div>
-    )
+class CityComponent extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            country: "",
+            city: "",
+            image: "",
+            description: "",
+            rating: "",
+            editing: false
+        }
+        this.handleEdit = this.handleEdit.bind(this)
+        this.showHideEdit = this.showHideEdit.bind(this)
+    }
+    componentDidMount(){
+        this.setState({
+            country: this.props.item.country,
+            city: this.props.item.city,
+            image: this.props.item.image,
+            description: this.props.item.description,
+            rating: this.props.item.rating
+        })
+    }
+    showHideEdit(){
+        this.setState(prevState => {
+            return({
+                editing: !prevState.editing
+            })
+        })
+    }
+    handleEdit(event){
+        const {name, value} = event.target
+        this.setState(prevState => {
+            return({
+                [name]: value,
+            })
+        })
+    }
+    render(){
+        return(
+            <div>
+                <h3>{this.state.country + "     " + this.state.city}</h3>
+                <img src={this.state.image} alt="something"></img>
+                {this.state.editing
+                    ?   <div>
+                            <form>
+                                <input 
+                                    name="country" 
+                                    value={this.state.country} 
+                                    onChange={this.handleEdit} 
+                                    placeholder="Country" 
+                                />
+                                <br />
+                                
+                                <input 
+                                    name="city" 
+                                    value={this.state.city}
+                                    onChange={this.handleEdit} 
+                                    placeholder="City" 
+                                />
+                                <br />
+                                <input 
+                                    name="image" 
+                                    value={this.state.image}
+                                    onChange={this.handleEdit} 
+                                    placeholder="Image" 
+                                />
+                                <br />
+                                <textarea 
+                                    name="description" 
+                                    value={this.state.description}
+                                    onChange={this.handleEdit} 
+                                    placeholder="Description" 
+                                />
+                                <br />
+                                <input 
+                                    name="rating" 
+                                    value={this.state.rating}
+                                    onChange={this.handleEdit} 
+                                    placeholder="Rating"
+                                />
+                                <br />
+                                <button onClick= {this.showHideEdit}>Save</button>
+                            </form>
+                        </div>
+                    :   <div>
+                            <p>{this.state.description}</p>
+                            <p>Rating: {this.state.rating}</p> 
+                            <button onClick={this.showHideEdit}>Edit</button>
+                        </div>
+                }
+            </div>
+        )
+    }
+        
+    
+    
     
 }
 
